603076e5244b331017b92d578c7aec64db2dede3
HristiyanZahariev <ihzahariev@gmail.com>
2017-05-09 03:25:19 +0300

    working on steps still

--- index.js
+++ index.js
@@ -26,7 +26,10 @@ var pool  = mysql.createPool({
 });
 
 app.get('/', function(request, response) {
-  response.send(pool.query('SELECT * FROM users WHERE ?', [{id: 1}]));
+  pool.query('SELECT * FROM users WHERE ?', [{id: 1}], function (err, result) {
+    if (err) throw err;
+    console.log("You have made " + result + "steps");
+  });
 
 });
 
